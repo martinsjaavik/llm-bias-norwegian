260492e02a2386fded3a612dbe01d69713ad1875
Martin Sjåvik <116551751+martinsjaavik@users.noreply.github.com>
2024-11-20 14:45:46 +0100

    Add files via upload

---
+++ local.py
@@ -0,0 +1,20 @@
+import requests
+
+def generate_response_local(model: str, query: str) -> str:
+    url = "http://localhost:11434/api/chat"
+    data = {
+        "model": model,
+        "messages": [
+            {
+                "role": "user",
+                "content": query
+            }
+        ],
+        "stream": False,
+        "options": {
+            "max_tokens": 50
+        }
+    }
+
+    response = requests.post(url, json=data)
+    return response.json()['message']['content']
---
+++ remote.py
@@ -0,0 +1,12 @@
+from litellm import completion
+import litellm
+litellm.set_verbose=True
+
+def generate_response_api(model: str, query: str) -> str:
+    response = completion(
+            model=model, 
+            max_tokens=50,
+            messages = [{ "content": query,"role": "user"}]
+        )
+
+    return response.choices[0].message.content
